7e6204111b43ca59594e10f77e8f65ed05680e90
Ann Maria Jose <annmariajose1412@gmail.com>
2024-03-12 23:18:45 -0500

    Update submit button

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ router.get('/', function(req, res) {
     res.render('form', { title: 'Registration form'});
 });
 
+router.post('/', function(req, res) {
+    res.render('form', { title: 'Registration form'});
+});
+
 module.exports = router;
