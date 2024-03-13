97836dc565330f1457fb74f23cd76d7150d60027
Ann Maria Jose <annmariajose1412@gmail.com>
2024-03-12 23:31:51 -0500

    Add validation

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,34 @@
+const { error } = require('console');
 const express = require('express');
+const {check, validationResult} = require('express-validator');
 const router = express.Router();
 
 router.get('/', function(req, res) {
     res.render('form', { title: 'Registration form'});
 });
 
-router.post('/', function(req, res) {
-    console.log(req.body);
-    res.render('form', { title: 'Registration form'});
-});
+router.post('/', 
+    [
+        check('name')
+            .isLength({min: 1})
+            .withMessage('Please enter a name'),
+        check('email')
+            .isLength({min: 1})
+            .withMessage('Please enter an email'),
+    ],
+    function(req, res) {
+        //console.log(req.body);
+        const errors = validationResult(req);
+        if (errors.isEmpty()) {
+            res.send('Thank you for your registration!');
+        } else {
+            res.render('form', {
+                title: 'Registration form',
+                errors: errors.array(),
+                data: req.body,
+            });
+        }
+    }
+);
 
 module.exports = router;
