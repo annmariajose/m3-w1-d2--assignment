a1d45f2b77426cd7a430bb6bc98f47fe44976e43
Ann Maria Jose <annmariajose1412@gmail.com>
2024-03-19 20:28:26 -0700

    Save data to the database

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const { error } = require('console');
 const express = require('express');
+const mongoose = require('mongoose');
 const {check, validationResult} = require('express-validator');
+
 const router = express.Router();
+const Registration = mongoose.model('Registration');
 
 router.get('/', function(req, res) {
     res.render('form', { title: 'Registration form'});
@@ -20,7 +23,13 @@ router.post('/',
         //console.log(req.body);
         const errors = validationResult(req);
         if (errors.isEmpty()) {
-            res.send('Thank you for your registration!');
+            const registration = new Registration(req.body);
+            registration.save()
+                .then(() => { res.send('Thank you for your registration!'); })
+                .catch((err) => {
+                    console.log(err);
+                    res.send('Sorry! Something went wrong.')
+                });
         } else {
             res.render('form', {
                 title: 'Registration form',
